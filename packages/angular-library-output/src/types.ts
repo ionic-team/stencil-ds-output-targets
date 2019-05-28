0937cd49b0350bf8e651dda6418f17f15ac5d666
Josh Thomas <josh@ionic.io>
2019-05-28 13:15:32 -0500

    add some docs and a build script for lerna

--- packages/angular-library-output/src/types.ts
+++ packages/angular-library-output/src/types.ts
@@ -9,6 +9,50 @@ export interface OutputTargetAngular {
 
 export type ValueAccessorTypes = 'text' | 'radio' | 'select' | 'number' | 'boolean';
 
+/*
+  const EVENTS = {
+    'Select': 'ionSelect',
+    'Change': 'ionChange'
+  };
+
+  const ATTRS = {
+    'Checked': 'checked',
+    'Value': 'value'
+  };
+
+  const angularValueAccessorBindings: ValueAccessorConfig[] = [
+    {
+      elementSelectors: [ 'ion-checkbox', 'ion-toggle' ],
+      event: EVENTS.Change,
+      targetAttr: ATTRS.Checked,
+      type: 'boolean'
+    },
+    {
+      elementSelectors: [ 'ion-input[type=number]' ],
+      event: EVENTS.Change,
+      targetAttr: ATTRS.Value,
+      type: 'number'
+    },
+    {
+      elementSelectors: [ 'ion-radio' ],
+      event: EVENTS.Select,
+      targetAttr: ATTRS.Checked,
+      type: 'radio'
+    },
+    {
+      elementSelectors: [ 'ion-range', 'ion-select', 'ion-radio-group', 'ion-segment', 'ion-datetime' ],
+      event: EVENTS.Change,
+      targetAttr: ATTRS.Value,
+      type: 'select'
+    },
+    {
+      elementSelectors: ['ion-input:not([type=number])', 'ion-textarea', 'ion-searchbar'],
+      event: EVENTS.Change,
+      targetAttr: ATTRS.Value,
+      type: 'text'
+    },
+  ];
+*/
 export interface ValueAccessorConfig {
   elementSelectors: string | string[];
   event: string;
