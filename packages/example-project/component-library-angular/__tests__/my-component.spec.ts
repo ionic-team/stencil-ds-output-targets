459c3b3afdcd87611f2844f87357fe368075471e
Josh Thomas <josh@ionic.io>
2020-06-08 15:25:13 -0500

    Add more angular tests.

--- packages/example-project/component-library-angular/__tests__/my-component.spec.ts
+++ packages/example-project/component-library-angular/__tests__/my-component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture } from '@angular/core/testing';
 import { ConfigureFn, configureTests } from '../src/config.testing';
 import { MyComponent } from './../src/index';
 
-describe('SimpleComponent', () => {
+describe('MyComponent', () => {
   let component: MyComponent;
   let fixture: ComponentFixture<MyComponent>;
 
@@ -22,6 +22,26 @@ describe('SimpleComponent', () => {
   }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const { nativeElement: myComponent, componentInstance: myAngularComponent } = fixture;
+    myAngularComponent.age = 39;
+    expect(myComponent.age).toEqual(39);
+  });
+
+  it('should get strings as props', () => {
+    const { nativeElement: myComponent, componentInstance: myAngularComponent } = fixture;
+    myAngularComponent.first = 'blue';
+    expect(myComponent.first).toEqual('blue');
+  });
+
+  it('should get numbers as props', () => {
+    const { nativeElement: myComponent, componentInstance: myAngularComponent } = fixture;
+    myAngularComponent.age = 39;
+    expect(myComponent.age).toEqual(39);
+  });
+
+  it('should get arrays as props', () => {
+    const { nativeElement: myComponent, componentInstance: myAngularComponent } = fixture;
+    myAngularComponent.kidsNames = ['billy', 'jane'];
+    expect(myComponent.kidsNames).toEqual(['billy', 'jane']);
   });
 });
