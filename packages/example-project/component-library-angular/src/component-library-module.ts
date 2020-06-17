b1919e7c65c009fdfced3c5b7859d702690e74fb
Josh Thomas <josh@ionic.io>
2020-06-17 12:58:22 -0500

    Add missing ngmodule declarations.

--- packages/example-project/component-library-angular/src/component-library-module.ts
+++ packages/example-project/component-library-angular/src/component-library-module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { defineCustomElements } from 'component-library/loader';
 
+import { BooleanValueAccessor } from './directives/boolean-value-accessor';
+import { NumericValueAccessor } from './directives/number-value-accessor';
+import { RadioValueAccessor } from './directives/radio-value-accessor';
+import { SelectValueAccessor } from './directives/select-value-accessor';
 import { TextValueAccessor } from './directives/text-value-accessor';
-import { MyComponent } from './directives/proxies';
+import {
+  MyComponent,
+  MyButton,
+  MyCheckbox,
+  MyInput,
+  MyPopover,
+  MyRadio,
+  MyRadioGroup,
+  MyRange,
+} from './directives/proxies';
 
 defineCustomElements(window);
 
 const DECLARATIONS = [
   // proxies
   MyComponent,
+  MyButton,
+  MyCheckbox,
+  MyInput,
+  MyPopover,
+  MyRadio,
+  MyRadioGroup,
+  MyRange,
 
   // Value Accessors
+  BooleanValueAccessor,
+  NumericValueAccessor,
+  RadioValueAccessor,
+  SelectValueAccessor,
   TextValueAccessor,
 ];
 
