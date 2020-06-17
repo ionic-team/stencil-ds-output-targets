fdecf853892c92045634dcb608fb96cd7a05ba8a
Josh Thomas <josh@ionic.io>
2020-06-17 13:07:39 -0500

    Add number value accessor test.

--- packages/example-project/component-library-angular/__tests__/my-input.spec.ts
+++ packages/example-project/component-library-angular/__tests__/my-input.spec.ts
@@ -9,25 +9,25 @@ import { ComponentLibraryModule } from '../src/index';
 @Component({
   template: `<my-input type="text" [(ngModel)]="testText"></my-input>`,
 })
-class TestHoverFocusComponent {
+class TestTextValueAccessorComponent {
   testText: string = '';
 }
 
-describe('MyInput', () => {
-  let component: TestHoverFocusComponent;
+describe('MyInput - Text Value', () => {
+  let component: TestTextValueAccessorComponent;
   let myInputEl: DebugElement;
-  let fixture: ComponentFixture<TestHoverFocusComponent>;
+  let fixture: ComponentFixture<TestTextValueAccessorComponent>;
 
   beforeEach(async(() => {
     const configure: ConfigureFn = (testBed) => {
       testBed.configureTestingModule({
         imports: [FormsModule, ComponentLibraryModule],
-        declarations: [TestHoverFocusComponent],
+        declarations: [TestTextValueAccessorComponent],
       });
     };
 
     configureTests(configure).then((testBed) => {
-      fixture = testBed.createComponent(TestHoverFocusComponent);
+      fixture = testBed.createComponent(TestTextValueAccessorComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
       myInputEl = fixture.debugElement.query(By.css('my-input'));
@@ -38,5 +38,46 @@ describe('MyInput', () => {
     const { componentInstance: myAngularComponent } = fixture;
     myInputEl.triggerEventHandler('myChange', { target: { value: 'text' } });
     expect(myAngularComponent.testText).toEqual('text');
+  });
+
+  it('on myChange type="number" the bound component attribute should update', () => {
+    const { componentInstance: myAngularComponent } = fixture;
+    myInputEl.triggerEventHandler('myChange', { target: { value: 'text' } });
+    expect(myAngularComponent.testText).toEqual('text');
+  });
+});
+
+@Component({
+  template: `<my-input type="number" [(ngModel)]="testNumber"></my-input>`,
+})
+class TestNumberValueAccessorComponent {
+  testNumber: number = 0;
+}
+
+describe('MyInput - Number Value', () => {
+  let component: TestNumberValueAccessorComponent;
+  let myInputEl: DebugElement;
+  let fixture: ComponentFixture<TestNumberValueAccessorComponent>;
+
+  beforeEach(async(() => {
+    const configure: ConfigureFn = (testBed) => {
+      testBed.configureTestingModule({
+        imports: [FormsModule, ComponentLibraryModule],
+        declarations: [TestNumberValueAccessorComponent],
+      });
+    };
+
+    configureTests(configure).then((testBed) => {
+      fixture = testBed.createComponent(TestNumberValueAccessorComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      myInputEl = fixture.debugElement.query(By.css('my-input'));
+    });
+  }));
+
+  it('on myChange type="number" the bound component attribute should update', () => {
+    const { componentInstance: myAngularComponent } = fixture;
+    myInputEl.triggerEventHandler('myChange', { target: { value: 42 } });
+    expect(myAngularComponent.testNumber).toEqual(42);
   });
 });
