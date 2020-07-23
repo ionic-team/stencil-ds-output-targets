32ba2d758d8a89cbb6a54bfa7698684359774822
Liam DeBeasi <liamdebeasi@icloud.com>
2020-07-23 10:16:36 -0400

    update

--- packages/example-project/component-library-vue3/src/vue-component-lib/utils.ts
+++ packages/example-project/component-library-vue3/src/vue-component-lib/utils.ts
@@ -1,11 +1,26 @@
 import { FunctionalComponent, h } from 'vue';
 
-export const defineContainer = <Props extends object>(name: string, componentProps: string[]) => {
-  const Container: FunctionalComponent<Props> = (props, { slots }) =>
-    h(name, props, slots.default && slots.default());
+export const defineContainer = <Props extends object>(name: string, componentProps: string[], modelProp?: string) => {
+  const Container: FunctionalComponent<Props> = (props, { slots }) => {
+    const finalProps = props;
+
+    if (modelProp) {
+      finalProps = {
+        ...props,
+        'modelValue': modelProp,
+
+      }
+    }
+    return h(
+      name,
+      finalProps,
+      slots.default && slots.default()
+    );
+  }
 
   Container.displayName = name;
   Container.props = componentProps;
+  Containers.emits = ['update:modelValue']
 
   return Container;
 };
