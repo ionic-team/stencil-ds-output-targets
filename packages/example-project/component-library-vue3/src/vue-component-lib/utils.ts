8128cd2ef3f04492d720eec50432f7e53575de6c
Liam DeBeasi <liamdebeasi@icloud.com>
2020-07-23 18:22:40 +0000

    test

--- packages/example-project/component-library-vue3/src/vue-component-lib/utils.ts
+++ packages/example-project/component-library-vue3/src/vue-component-lib/utils.ts
@@ -1,16 +1,21 @@
 import { FunctionalComponent, h } from 'vue';
 
+export interface InputProps extends Object {
+  modelValue: string | boolean;
+}
+
 export const defineContainer = <Props extends object>(name: string, componentProps: string[], modelProp?: string) => {
-  const Container: FunctionalComponent<Props> = (props, { slots }) => {
-    const finalProps = props;
-
-    if (modelProp) {
-      finalProps = {
-        ...props,
+  const Container: FunctionalComponent<Props & InputProps> = (props, { slots }) => {
+    const { modelValue, ...restOfProps } = props;
+    const finalProps = (modelProp) ? (
+      {
+        ...restOfProps,
         [modelProp]: props.hasOwnProperty('modelValue') ? modelValue : (props as any)[modelProp],
-
       }
-    }
+    ) : restOfProps;
+
+    console.log('processing',name,modelProp,finalProps);
+
     return h(
       name,
       finalProps,
@@ -21,7 +26,7 @@ export const defineContainer = <Props extends object>(name: string, componentPro
   Container.displayName = name;
   Container.props = componentProps;
   if (modelProp) {
-    Containers.emits = ['update:modelValue'];
+    Container.emits = ['update:modelValue'];
   }
 
   return Container;
