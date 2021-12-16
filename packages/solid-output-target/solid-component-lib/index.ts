4b28797964ddfba1ee28e854f9cf6a53968ba0e0
Martin Oppitz <github@martinoppitz.com>
2021-12-16 01:48:23 +0100

    chore: add children

--- packages/solid-output-target/solid-component-lib/index.ts
+++ packages/solid-output-target/solid-component-lib/index.ts
@@ -2,7 +2,11 @@ export function createComponent<Props>(tag: string) {
   return (props: Props) => {
     const node = document.createElement(tag);
     for (const key in props) {
-      if (Object.prototype.hasOwnProperty.call(props, key)) {
+      if (key === 'children' && Array.isArray(props[key])) {
+        (node as Record<string, any>)[key].forEach((child: HTMLElement) => {
+          node.appendChild(child);
+        })
+      } else if (Object.prototype.hasOwnProperty.call(props, key)) {
         (node as Record<string, any>)[key] = props[key];
       }
     }
