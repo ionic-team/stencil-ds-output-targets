bba2b6c2ddcc9d6e1d60db0aaae8a36e73500e0c
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-08-19 11:04:32 -0400

    chore(format): format svelte-output-target

--- packages/svelte-output-target/src/output-svelte.ts
+++ packages/svelte-output-target/src/output-svelte.ts
@@ -1,7 +1,5 @@
 import path from 'path';
-import type {
-  CompilerCtx, ComponentCompilerMeta, Config, OutputTargetDist,
-} from '@stencil/core/internal';
+import type { CompilerCtx, ComponentCompilerMeta, Config, OutputTargetDist } from '@stencil/core/internal';
 import { OutputTargetSvelte } from './types';
 import { sortBy, normalizePath, dashToPascalCase } from './utils';
 import { createComponentDefinition } from './generate-svelte-component';
@@ -13,44 +11,30 @@ const REGISTER_CUSTOM_ELEMENTS = 'defineCustomElements';
 const APPLY_POLYFILLS = 'applyPolyfills';
 const DEFAULT_LOADER_DIR = '/dist/loader';
 
-const ignoreChecks = () => [
-  '/* eslint-disable */',
-  '/* tslint:disable */',
-  '// @ts-nocheck',
-].join('\n');
+const ignoreChecks = () => ['/* eslint-disable */', '/* tslint:disable */', '// @ts-nocheck'].join('\n');
 
-const getFilteredComponents = (
-  excludeComponents: string[] = [],
-  cmps: ComponentCompilerMeta[],
-) => sortBy<ComponentCompilerMeta>(
-  cmps,
-  (cmp: ComponentCompilerMeta) => cmp.tagName,
-).filter(
-  (c: ComponentCompilerMeta) => !excludeComponents.includes(c.tagName) && !c.internal,
-);
+const getFilteredComponents = (excludeComponents: string[] = [], cmps: ComponentCompilerMeta[]) =>
+  sortBy<ComponentCompilerMeta>(cmps, (cmp: ComponentCompilerMeta) => cmp.tagName).filter(
+    (c: ComponentCompilerMeta) => !excludeComponents.includes(c.tagName) && !c.internal
+  );
 
 export const getPathToCorePackageLoader = (config: Config, outputTarget: OutputTargetSvelte) => {
   const basePkg = outputTarget.componentCorePackage || '';
   const distOutputTarget = config.outputTargets?.find((o) => o.type === 'dist') as OutputTargetDist;
 
-  const distAbsEsmLoaderPath = (distOutputTarget?.esmLoaderPath
-    && path.isAbsolute(distOutputTarget.esmLoaderPath))
-    ? distOutputTarget.esmLoaderPath
-    : null;
+  const distAbsEsmLoaderPath =
+    distOutputTarget?.esmLoaderPath && path.isAbsolute(distOutputTarget.esmLoaderPath)
+      ? distOutputTarget.esmLoaderPath
+      : null;
 
-  const distRelEsmLoaderPath = config.rootDir && distAbsEsmLoaderPath
-    ? path.relative(config.rootDir, distAbsEsmLoaderPath)
-    : null;
+  const distRelEsmLoaderPath =
+    config.rootDir && distAbsEsmLoaderPath ? path.relative(config.rootDir, distAbsEsmLoaderPath) : null;
 
   const loaderDir = outputTarget.loaderDir || distRelEsmLoaderPath || DEFAULT_LOADER_DIR;
   return normalizePath(path.join(basePkg, loaderDir));
 };
 
-export function generateProxies(
-  config: Config,
-  components: ComponentCompilerMeta[],
-  outputTarget: OutputTargetSvelte,
-) {
+export function generateProxies(config: Config, components: ComponentCompilerMeta[], outputTarget: OutputTargetSvelte) {
   const pathToCorePackageLoader = getPathToCorePackageLoader(config, outputTarget);
 
   let sourceImports = `import '${pathToCorePackageLoader}';`;
@@ -66,10 +50,8 @@ export function generateProxies(
 
   const fileName = (c: ComponentCompilerMeta) => dashToPascalCase(c.tagName);
 
-  const buildImports = (
-    dir: string,
-    ext = '',
-  ) => components.map((c) => `import ${fileName(c)} from '${dir}/${fileName(c)}${ext}';`).join('\n');
+  const buildImports = (dir: string, ext = '') =>
+    components.map((c) => `import ${fileName(c)} from '${dir}/${fileName(c)}${ext}';`).join('\n');
 
   const buildExports = () => components.map((c) => `export { ${fileName(c)} };`).join('\n');
 
@@ -87,11 +69,7 @@ export function generateProxies(
     content: createComponentDefinition(c, outputTarget.componentBindings),
   }));
 
-  const uncompiledEntry = [
-    ignoreChecks(),
-    buildImports('.', '.svelte'),
-    buildExports(),
-  ].join('\n');
+  const uncompiledEntry = [ignoreChecks(), buildImports('.', '.svelte'), buildExports()].join('\n');
 
   const compiledFiles = uncompiledFiles.map((file) => ({
     name: file.name,
@@ -118,7 +96,7 @@ export const svelteProxyOutput = async (
   config: Config,
   compilerCtx: CompilerCtx,
   outputTarget: OutputTargetSvelte,
-  components: ComponentCompilerMeta[],
+  components: ComponentCompilerMeta[]
 ) => {
   const filteredComponents = getFilteredComponents(outputTarget.excludeComponents, components);
 
@@ -130,23 +108,27 @@ export const svelteProxyOutput = async (
 
   await compilerCtx.fs.writeFile(path.resolve(uncompiledDir, 'index.js'), output.uncompiledEntry);
 
-  await Promise.all(output.uncompiledFiles.map((file) => {
-    const filePath = path.resolve(uncompiledDir, `${file.name}.svelte`);
-    return compilerCtx.fs.writeFile(filePath, file.content);
-  }));
+  await Promise.all(
+    output.uncompiledFiles.map((file) => {
+      const filePath = path.resolve(uncompiledDir, `${file.name}.svelte`);
+      return compilerCtx.fs.writeFile(filePath, file.content);
+    })
+  );
 
-  await Promise.all(output.compiledFiles.map((file) => {
-    const filePath = path.resolve(compiledDir, `${file.name}.ts`);
-    const { content, meta } = file;
+  await Promise.all(
+    output.compiledFiles.map((file) => {
+      const filePath = path.resolve(compiledDir, `${file.name}.ts`);
+      const { content, meta } = file;
 
-    return compilerCtx.fs.writeFile(
-      filePath,
-      [
-        ignoreChecks(),
-        `import { Components, JSX } from '${outputTarget.componentCorePackage}';\n`,
-        generateTypings(meta),
-        replaceMethodDefs(meta, generate$$TypeDefs(meta, content)),
-      ].join('\n'),
-    );
-  }));
+      return compilerCtx.fs.writeFile(
+        filePath,
+        [
+          ignoreChecks(),
+          `import { Components, JSX } from '${outputTarget.componentCorePackage}';\n`,
+          generateTypings(meta),
+          replaceMethodDefs(meta, generate$$TypeDefs(meta, content)),
+        ].join('\n')
+      );
+    })
+  );
 };
