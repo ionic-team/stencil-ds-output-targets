186bdc51b8be4c169bd87c5d655e3a890060806d
Sean Perkins <sean@ionic.io>
2024-04-02 23:50:25 -0400

    test(react): add webdriver.io

--- packages/example-project/component-library-react/src/App.tsx
+++ packages/example-project/component-library-react/src/App.tsx
@@ -1,16 +1,9 @@
-import { MyInput } from './components';
+import Input from './Input/Input';
 
 function App() {
   return (
     <div>
-      <h1>Component Library</h1>
-
-      <h3>MyInput</h3>
-      <MyInput
-        onMyInput={(ev) => console.log('myInput', ev.detail)}
-        onMyChange={(ev) => console.log('myChange', ev.detail)}
-        value={'initial value'}
-      />
+      <Input />
     </div>
   );
 }
