220c57d890b57d6d1b3bfc8afd93a261432ea3f4
Sean Perkins <sean@ionic.io>
2024-04-05 13:49:17 -0400

    chore: build with vite

---
+++ packages/react-output-target/vite.config.ts
@@ -0,0 +1,26 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        'react-output-target': 'src/react-output-target/index.ts',
+        react: 'src/react/index.ts',
+      },
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['@stencil/core', 'typescript', 'ts-morph'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'ts-morph': 'tsMorph',
+        },
+      },
+    },
+  },
+});
