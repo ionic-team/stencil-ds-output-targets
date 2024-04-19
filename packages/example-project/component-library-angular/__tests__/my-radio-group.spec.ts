adaf224ce9d31cb8e3bb5b6aa37a0c2bd8936147
Sean Perkins <sean@ionic.io>
2024-04-19 10:30:56 -0400

    fix: example angular library spec tests

--- packages/example-project/component-library-angular/__tests__/my-radio-group.spec.ts
+++ packages/example-project/component-library-angular/__tests__/my-radio-group.spec.ts
@@ -1,6 +1,5 @@
-import { async, ComponentFixture } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConfigureFn, configureTests } from '../src/config.testing';
 import { DebugElement, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
@@ -21,20 +20,16 @@ describe('MyRadioGroup', () => {
   let myRadioGroupEl: DebugElement;
   let fixture: ComponentFixture<TestRadioValueAccessorComponent>;
 
-  beforeEach(async(() => {
-    const configure: ConfigureFn = (testBed) => {
-      testBed.configureTestingModule({
-        imports: [FormsModule, ComponentLibraryModule],
-        declarations: [TestRadioValueAccessorComponent],
-      });
-    };
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule, ComponentLibraryModule],
+      declarations: [TestRadioValueAccessorComponent],
+    }).compileComponents();
 
-    configureTests(configure).then((testBed) => {
-      fixture = testBed.createComponent(TestRadioValueAccessorComponent);
-      fixture.detectChanges();
-      myRadioGroupEl = fixture.debugElement.query(By.css('my-radio-group'));
-    });
-  }));
+    fixture = TestBed.createComponent(TestRadioValueAccessorComponent);
+    fixture.detectChanges();
+    myRadioGroupEl = fixture.debugElement.query(By.css('my-radio-group'));
+  });
 
   it('on myChange value the bound component attribute should update', () => {
     const { componentInstance: myAngularComponent } = fixture;
